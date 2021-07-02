5c8cab92860c63a9353d6d0a65e7e6a388b2f9ae
charlieyang34 <yy3we@virginia.edu>
2021-07-02 00:51:23 -0400

    new branch

--- src/App.js
+++ src/App.js
@@ -4,39 +4,31 @@
 //         React.createElement("h3", {}, "Dog"),
 //         React.createElement("h3", {}, "Havanese")
 //     ]);
-// };
-
-// const App = () => {
-//     return React.createElement(
-//         "div",
-//         {},
-//         [
-//             React.createElement("h1",{}, "Adopt Me!", {}),
-//             React.createElement(Pet),
-//             React.createElement(Pet),
-//             React.createElement(Pet),
-//         ]
-//     );
-// };
-
-// ReactDOM.render(React.createElement(App), document.getElementById('root'));
-
-
-
-const Pet = () => {
+const Pet = (props) => {
     return React.createElement("div", {}, [
-      React.createElement("h1", {}, "Luna"),
-      React.createElement("h2", {}, "Dog"),
-      React.createElement("h2", {}, "Havanese"),]);
+      React.createElement("h1", {}, props.name),
+      React.createElement("h2", {}, props.animal),
+      React.createElement("h2", {}, props.breed),
+    ]);
   };
   
   const App = () => {
     return React.createElement("div", {}, [
       React.createElement("h1", {}, "Adopt Me!"),
-      React.createElement(Pet),
-      React.createElement(Pet),
-      React.createElement(Pet),
+      React.createElement(Pet, {
+        name: "Luna",
+        animal: "Dog",
+        breed: "Havanese",
+      }),
+      React.createElement(Pet, {
+        name: "Pepper",
+        animal: "Bird",
+        breed: "Cockatiel",
+      }),
+      React.createElement(Pet, { name: "Doink", animal: "Cat", breed: "Mix" }),
     ]);
   };
   
   ReactDOM.render(React.createElement(App), document.getElementById("root"));
+
+  //above are the old fashioned way of writing React, which are now replaced by Jsx 
