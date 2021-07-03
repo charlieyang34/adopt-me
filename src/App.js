35d7f7914926ca7508cd3513d7e8c3b673bac8e4
charlieyang34 <yy3we@virginia.edu>
2021-07-03 00:11:32 -0400

    dev ready setup

--- src/App.js
+++ src/App.js
@@ -1,34 +1,24 @@
-// const Pet = () => {
-//     return React.createElement("div", {}, [
-//         React.createElement("h1",{}, "Luna"),
-//         React.createElement("h3", {}, "Dog"),
-//         React.createElement("h3", {}, "Havanese")
-//     ]);
-const Pet = (props) => {
-    return React.createElement("div", {}, [
-      React.createElement("h1", {}, props.name),
-      React.createElement("h2", {}, props.animal),
-      React.createElement("h2", {}, props.breed),
-    ]);
-  };
-  
-  const App = () => {
-    return React.createElement("div", {}, [
-      React.createElement("h1", {}, "Adopt Me!"),
-      React.createElement(Pet, {
-        name: "Luna",
-        animal: "Dog",
-        breed: "Havanese",
-      }),
-      React.createElement(Pet, {
-        name: "Pepper",
-        animal: "Bird",
-        breed: "Cockatiel",
-      }),
-      React.createElement(Pet, { name: "Doink", animal: "Cat", breed: "Mix" }),
-    ]);
-  };
-  
-  ReactDOM.render(React.createElement(App), document.getElementById("root"));
+import React from "react";
+import ReactDOM from "react-dom";
+import Pet from "./Pet";
 
-  //above are the old fashioned way of writing React, which are now replaced by Jsx 
+const App = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet, {
+      name: "Luna",
+      animal: "Dog",
+      breed: "Havanese",
+    }),
+    React.createElement(Pet, {
+      name: "Pepper",
+      animal: "Bird",
+      breed: "Cockatiel",
+    }),
+    React.createElement(Pet, { name: "Doink", animal: "Cat", breed: "Mix" }),
+  ]);
+};
+
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
+
+//above are the old fashioned way of writing React, which are now replaced by Jsx
